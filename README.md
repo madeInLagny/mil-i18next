8ddd9fcfa92be1585402ae9691e6dc8c3085bf96
madeInLagny <madeInLagny@gmail.com>
2019-08-28 07:17:37 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,21 +1,58 @@
 # \<mil-i18next\>
 
-Javascript i18next mixin for webcomponents
+'mil-i18next' is a javascript mixin that implements [i18next](https://www.i18next.com/) in webcomponents and litElements.
+It provides translation upfront and lazy-loading and automatic language detection.
 
-## Install the Polymer-CLI
+## Usage
 
-First, make sure you have the [Polymer CLI](https://www.npmjs.com/package/polymer-cli) and npm (packaged with [Node.js](https://nodejs.org)) installed. Run `npm install` to install your element's dependencies, then run `polymer serve` to serve your element locally.
+### Install from npm
 
-## Viewing Your Element
-
-```
-$ polymer serve
-```
-
-## Running Tests
-
-```
-$ polymer test
+```sh
+npm install --save mil-i18next
 ```
 
-Your application is already set up to be tested via [web-component-tester](https://github.com/Polymer/web-component-tester). Run `polymer test` to run your application's test suite locally.
+### Import to your webcomponent
+
+```js
+import { i18nMixin } from "mil-i18next";
+```
+
+### Extend your class
+
+```js
+class myClass extends i18nMixin(...)
+```
+
+### Optionnally override default properties
+
+```js
+constructor() {
+    super();
+    this.ns = ["app", "common", "notification"]; /* default: ["app"] */
+    this.languageResources = '/path to translation files/{{lng}}/{{ns}}.json' /* default: '/assets/locales/{{lng}}/{{ns}}.json' */
+    this.fallbackLng = "fr"; /* defaut: "en" */
+    this.defaultNS = ["app"]; /* defaut: ["app"] */
+    this.i18nextResources = {
+      en: {
+        app: {
+          key1: "Translations keyed here will be loaded before i18next renders",
+          key2: "Use language files for lazy-loading",
+          }
+      },
+      fr: {
+        app: {
+          key1: "Les valeurs saisies ici seront chargées avant l'initialisation d'i18next",
+          key2: "Les valeurs qui peuvent être chargées sans bloquer l'initialisation doivent être saisies dans les fichiers de traduction.",
+         }
+       }
+    };
+  }
+```
+
+Translation file load is asynchronous. Any translations that can be lazy-loaded should be keyed in translation files. To avoid unpleasant text flashing, all other translations should be keyed in 'this.i18nextResources'
+
+### Use in your element
+
+```js
+${this.i18n.t("app:key1")}
+```
--- mil-i18next.js
+++ mil-i18next.js
@@ -1,32 +1,60 @@
-import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import i18next, { t as translate } from "i18next";
+import backend from "i18next-xhr-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+export { translate };
 
-/**
- * `mil-i18next`
- * Javascript i18next mixin for webcomponents
- *
- * @customElement
- * @polymer
- * @demo demo/index.html
- */
-class MilI18next extends PolymerElement {
-  static get template() {
-    return html`
-      <style>
-        :host {
-          display: block;
-        }
-      </style>
-      <h2>Hello [[prop1]]!</h2>
-    `;
-  }
-  static get properties() {
-    return {
-      prop1: {
-        type: String,
-        value: 'mil-i18next',
-      },
-    };
-  }
-}
+export const i18nMixin = baseClass =>
+  class extends baseClass {
+    static get properties() {
+      return {
+        /** Namespaces to be loaded. */
+        ns: { type: Array },
+        /**Keys to be loaded before i18next renders. */
+        i18nextResources: { type: Object },
+        /**Fallback language. */
+        fallbackLng: { type: String },
+        /**Default namespace. */
+        defaultNS: { type: String },
+        /**Path to translation files. */
+        languageResources: { type: String },
+        /**I18 object. */
+        i18n: { type: Object }
+      };
+    }
 
-window.customElements.define('mil-i18next', MilI18next);
+    connectedCallback() {
+      super.connectedCallback();
+
+      this.i18n = i18next.createInstance();
+      this.i18n.on("initialized", options => {
+        this.requestUpdate();
+      });
+      this.i18n.on("languageChanged", options => {
+        this.requestUpdate();
+      });
+      this.i18n
+        .use(backend)
+        .use(LanguageDetector)
+        .init({
+          debug: true,
+          defaultNS: this.defaultNS || "app",
+          ns: this.ns || ["app"],
+          fallbackLng: this.fallbackLng || "en",
+          partialBundledLanguages: true,
+          resources: this.i18nextResources,
+          backend: {
+            loadPath:
+              this.languageResources || "/bassets/locales/{{lng}}/{{ns}}.json"
+          }
+        });
+    }
+
+    /**
+     * Changes language after initialisation.
+     *
+     * @param {String} lang Language to apply.
+     */
+    changeLanguage(lang) {
+      this.i18n.changeLanguage(lang);
+    }
+  };
